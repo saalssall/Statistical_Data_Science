040a668e5feb483cd29185570b41b684ceb33d62
Hamidullah Rezae <hamidullahrezae15@gmail.com>
2026-06-18 16:08:17 +1000

    Add an example for wait times

--- Exercises/Exercises_2.r
+++ Exercises/Exercises_2.r
@@ -136,3 +136,21 @@ expected_val
 size_distribution |>
   filter(group_size >= 4) |>
   summarize(prob_4_or_more = sum(probability))
+
+
+# Min and max wait times for back-up that happens every 30 min
+min <- 0
+max <- 30
+
+# Calculate probability of waiting less than 5 mins
+prob_less_than_5 <- punif(5, min, max)
+prob_less_than_5
+
+# Calculate probability of waiting more than 5 mins
+prob_greater_than_5 <- punif(5, min, max, lower.tail = FALSE)
+prob_greater_than_5
+
+# Calculate probability of waiting 10-20 mins
+prob_between_10_and_20 <- punif(20, min, max) - punif(10, min, max)
+prob_between_10_and_20 
+
