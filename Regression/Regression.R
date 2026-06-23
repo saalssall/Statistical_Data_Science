2a3713ff6e11df752e1d623abb7d1124899a5127
Hamidullah Rezae <hamidullahrezae15@gmail.com>
2026-06-23 15:49:51 +1000

    Use the churn dataset and add MonthlyCharges histogram faceted by tenure group for churn dataset

--- Regression/Regression.R
+++ Regression/Regression.R
@@ -7,7 +7,8 @@ install.packages("ggplot2")
 library(dplyr)
 library(ggplot2)
 library(readr)
-getwd()
+df <- read_csv("Data/telecom_churn.csv")
+head(df, 10)
 
 # Add a linear trend line without a confidence ribbon
 ggplot(mtcars, aes(wt, mpg)) +
@@ -17,3 +18,15 @@ ggplot(mtcars, aes(wt, mpg)) +
 # Using mtcars to calculate the slope and intercept of the linear regression
 model_1 <- lm(mpg ~ wt, data = mtcars)
 model_1
+
+#Intercept (37.285) — when weight is 0, predicted mpg is 37.285. 
+#wt (-5.344) — for every 1 unit increase in weight (1000 lbs), mpg decreases by 5.344. 
+#So a heavier car gets significantly worse fuel efficiency.
+
+# Using df, plot monthlycharges
+ggplot(df, aes(MonthlyCharges)) +
+  # Make it a histogram with 10 bins
+  geom_histogram(bins = 10) +
+  # Facet the plot so each house age group gets its own panel
+  facet_wrap(vars(PhoneService))
+
